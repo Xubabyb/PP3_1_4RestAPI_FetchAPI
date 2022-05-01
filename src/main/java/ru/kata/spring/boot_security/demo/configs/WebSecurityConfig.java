e3aa8f20af4a86448e4bd32f939c33835aced54a
DmitriyShidlouskij <xubabyb@gmail.com>
2022-05-01 19:53:38 +0300

    add handler fix SecurityConfig

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -1,47 +1,71 @@
 package ru.kata.spring.boot_security.demo.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import ru.kata.spring.boot_security.demo.configs.handler.CustomAccessDeniedHandler;
+import ru.kata.spring.boot_security.demo.configs.handler.CustomAuthenticationFailureHandler;
+import ru.kata.spring.boot_security.demo.configs.handler.CustomAuthenticationSuccessHandler;
+import ru.kata.spring.boot_security.demo.configs.handler.CustomUrlLogoutSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    private final SuccessUserHandler successUserHandler;
+
 
     private final UserDetailsService userDetailsService;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
+    private final CustomAuthenticationFailureHandler customAuthenticationFailureHandler;
+    private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
+    private final CustomUrlLogoutSuccessHandler customUrlLogoutSuccessHandler;
 
-    public WebSecurityConfig(SuccessUserHandler successUserHandler,
-                             @Qualifier("userDetailsServiceImpl") UserDetailsService userDetailsService) {
-        this.successUserHandler = successUserHandler;
+    @Autowired
+    public WebSecurityConfig(
+            @Qualifier("userDetailsServiceImpl") UserDetailsService userDetailsService,
+            CustomAccessDeniedHandler customAccessDeniedHandler,
+            CustomAuthenticationFailureHandler customAuthenticationFailureHandler,
+            CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler,
+            CustomUrlLogoutSuccessHandler customUrlLogoutSuccessHandler) {
         this.userDetailsService = userDetailsService;
+        this.customAccessDeniedHandler = customAccessDeniedHandler;
+        this.customAuthenticationFailureHandler = customAuthenticationFailureHandler;
+        this.customAuthenticationSuccessHandler = customAuthenticationSuccessHandler;
+        this.customUrlLogoutSuccessHandler = customUrlLogoutSuccessHandler;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
+        http.csrf().disable()
+                // Декларирует, что все запросы к любой конечной точке должны быть авторизованы, иначе они должны быть отклонены
                 .authorizeRequests()
-                .antMatchers("/admin/**")
-                .hasRole("ADMIN")
-                .anyRequest()
-                .authenticated()
+                .antMatchers("/", "/css/**", "/js/**", "/webjars/**").permitAll()
+                .antMatchers("/api/users/*", "/api/roles").hasRole("ADMIN")
+                .anyRequest().authenticated()
                 .and()
-                .formLogin().successHandler(successUserHandler)
-                .loginPage("/login")
+                .exceptionHandling().accessDeniedHandler(customAccessDeniedHandler);
+        http.formLogin()
+                .loginPage("/") // указываем страницу с формой логина
+                .permitAll() // даем доступ к форме логина всем
+                .loginProcessingUrl("/login")
+                .successHandler(customAuthenticationSuccessHandler)
+                .failureHandler(customAuthenticationFailureHandler);
+        http.logout()
+                .logoutUrl("/logout")
+                .clearAuthentication(true)
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+                .logoutSuccessUrl("/")
+                .logoutSuccessHandler(customUrlLogoutSuccessHandler)
                 .permitAll()
-                .and()
-                .logout()
-                .permitAll();
+        ;
     }
 
     @Bean
@@ -49,11 +73,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder(12);
     }
 
-    @Bean
-    protected DaoAuthenticationProvider daoAuthenticationProvider() {
-        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
-        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
-        return daoAuthenticationProvider;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
+
 }
