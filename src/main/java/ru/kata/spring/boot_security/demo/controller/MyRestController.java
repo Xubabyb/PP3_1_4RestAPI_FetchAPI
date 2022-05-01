83d25431db704add88eb79cf2bdeefcd868867c7
DmitriyShidlouskij <xubabyb@gmail.com>
2022-05-01 17:56:56 +0300

    хз нибер достает юзеров но уже нет на экране

--- src/main/java/ru/kata/spring/boot_security/demo/controller/MyRestController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/MyRestController.java
@@ -2,7 +2,9 @@ package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.RoleService;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
 import java.util.List;
@@ -10,11 +12,19 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class MyRestController {
+
     private final UserService userService;
+    private final RoleService roleService;
 
     @Autowired
-    public MyRestController(UserService userService) {
+    public MyRestController(UserService userService, RoleService roleService) {
         this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @GetMapping("/roles")
+    public List<Role> findAllRoles() {
+        return roleService.getAllRoles();
     }
 
     @GetMapping("/users")
