b66e1953450ba2c553e0d10daa3d666d74a5f799
DmitriyShidlouskij <xubabyb@gmail.com>
2022-05-01 10:06:29 +0300

    add MyRestController (not check Exception)

--- src/main/java/ru/kata/spring/boot_security/demo/controller/MyRestController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/MyRestController.java
@@ -1,7 +1,48 @@
 package ru.kata.spring.boot_security.demo.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class MyRestController {
+    private final UserService userService;
+
+    @Autowired
+    public MyRestController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public List<User> showAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping("/users")
+    public User addNewUser(@RequestBody User user) {
+        userService.saveUser(user);
+        return user;
+    }
+
+    @PutMapping("/users")
+    public User updateUser(@RequestBody User user) {
+        userService.saveUser(user);
+        return user;
+    }
+
+    @DeleteMapping("/users/{id}")
+    public String deleteUser(@PathVariable Long id) {
+        userService.removeUserById(id);
+        return "User with ID= " + id + "was deleted";
+    }
+
 }
